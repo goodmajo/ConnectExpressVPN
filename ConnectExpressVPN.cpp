864f0f008845549d55b83d68920a46956a1b1e86
Joel Goodman <joelrgoodman@gmail.com>
2020-09-19 16:02:12 -0400

    Removed the need for .vpnstate. Getting the result of [1;32;49mConnected to USA - Washington DC [0m    - If your VPN connection unexpectedly drops, internet traffic will be blocked to protect your privacy.    - To disable Network Lock, disconnect ExpressVPN then type 'expressvpn preferences set network_lock off'. is fast enough and I don't really mind the very minor string comparison. Plus, this is now robust to reboots (which could leave the state file with incorrect information.)

--- ConnectExpressVPN.cpp
+++ ConnectExpressVPN.cpp
@@ -16,30 +16,18 @@ int main()
     const char* fname                = ".vpnstate";
     const char* vpnConnectCommand    = "expressvpn connect smart";
     const char* vpnDisconnectCommand = "expressvpn disconnect";
-
-    bool connected;
-    std::ifstream stateFileIn;
-    stateFileIn.open(fname);
-    
-    if(!stateFileIn.is_open())
-    {
-	/* Credit for the code below that gets command output belongs to gregpaton08 on SO (https://bit.ly/3iMTppU).
-	 * I've been using this solution for years now and it always works perfectly. Thanks, Greg! */
-    	std::array<char,1023> buffer;
-	const char* statusCommand = "expressvpn status";
-    	std::string result;
-    	std::unique_ptr<std::FILE,decltype(&pclose)> pipe(popen(statusCommand, "r"), pclose);
-    	if (!pipe)
-       		throw std::runtime_error("popen() failed!");
-    	while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-        	result += buffer.data();
-    	connected = (result.substr(0,3) == "Not") ? false : true;
-    }
-    else
-    {
-    	stateFileIn >> connected;
-    	stateFileIn.close();
-    }
+ 
+    /* Credit for the code below that gets command output belongs to gregpaton08 on SO (https://bit.ly/3iMTppU).
+     * I've been using this solution for years now and it always works perfectly. Thanks, Greg! */
+    std::array<char,1023> buffer;
+    const char* statusCommand = "expressvpn status";
+    std::string result;
+    std::unique_ptr<std::FILE,decltype(&pclose)> pipe(popen(statusCommand, "r"), pclose);
+    if (!pipe)
+        throw std::runtime_error("popen() failed!");
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+        result += buffer.data();
+    const bool connected = (result.substr(0,3) == "Not") ? false : true;
 
     if(connected)
     {
@@ -51,12 +39,6 @@ int main()
         std::system("notify-send -i face-cool \"VPN Connector\" \"Connecting ExpressVPN...\"");
         std::system(vpnConnectCommand);
     }
-
-    std::ofstream stateFileOut;
-    stateFileOut.open(fname, std::ios::trunc);
-    const char* newState = connected ? "0" : "1";
-    stateFileOut << newState;
-    stateFileOut.close();
 
     return EXIT_SUCCESS;
 }
