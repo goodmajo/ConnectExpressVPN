1e2c9b87c57a183e0dbbed64d6024efbac72944f
Joel Goodman <joelrgoodman@gmail.com>
2020-09-19 16:40:08 -0400

    Only getting the number of characters from command line output that we check to confirm connection state. Also, constexpr everything at the top.

--- ConnectExpressVPN.cpp
+++ ConnectExpressVPN.cpp
@@ -3,31 +3,38 @@
     Joel Goodman 2020
 */
 
+
 #include <iostream>
-#include <fstream>
 #include <cstring>
 #include <cstdlib>
 #include <string>
 #include <memory>
+#include <vector>
 
 
 int main()
 {
-    const char* fname                = ".vpnstate";
-    const char* vpnConnectCommand    = "expressvpn connect smart";
-    const char* vpnDisconnectCommand = "expressvpn disconnect";
- 
+    constexpr const char* vpnConnectCommand    = "expressvpn connect smart";
+    constexpr const char* vpnDisconnectCommand = "expressvpn disconnect";
+    constexpr const char* notConnectedSubStr   = "Not";	/* This is a substring we check for to tell whether or not vpn is connected. */     
+    constexpr const char* statusCommand        = "expressvpn status";
+    constexpr size_t subStrLen	               = strlen(notConnectedSubStr);
+
     /* Credit for the code below that gets command output belongs to gregpaton08 on SO (https://bit.ly/3iMTppU).
      * I've been using this solution for years now and it always works perfectly. Thanks, Greg! */
-    std::array<char,1023> buffer;
-    const char* statusCommand = "expressvpn status";
+    std::vector<char> buffer;
+    buffer.reserve(subStrLen);
+
     std::string result;
+    result.reserve(subStrLen);
+
     std::unique_ptr<std::FILE,decltype(&pclose)> pipe(popen(statusCommand, "r"), pclose);
     if (!pipe)
         throw std::runtime_error("popen() failed!");
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+    while (fgets(buffer.data(), subStrLen, pipe.get()) != nullptr)
         result += buffer.data();
-    const bool connected = (result.substr(0,3) == "Not") ? false : true;
+
+    const bool connected = (result.substr(0,subStrLen) == notConnectedSubStr) ? false : true;
 
     if(connected)
     {
@@ -42,4 +49,5 @@ int main()
 
     return EXIT_SUCCESS;
 }
+
 
